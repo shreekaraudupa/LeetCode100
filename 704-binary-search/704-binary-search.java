a4781d376306cc14126ae38c5a158a9117321b6d
shreekara  udupa <shreekaraudupa@gmail.com>
2022-04-18 18:46:46 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (80.54%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+   public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start < end){
+            int mid = (end - start)/2 + start;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                end = mid;
+            }else{
+                start = mid +1;
+            }
+        }
+        
+        if(nums[start] == target){
+            return start;
+        }
+        return -1;
+    }
+}
