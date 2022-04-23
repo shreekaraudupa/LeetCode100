d0de05c0c7e163c66a9505a88cbd6d5bfc023c9e
shreekara  udupa <shreekaraudupa@gmail.com>
2022-04-23 23:57:15 +0530

    Time: 1 ms (33.77%), Space: 42.2 MB (62.85%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        int tmp;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                tmp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = tmp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }else if(nums[mid] == 2){
+                tmp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = tmp;
+                high--;
+            }
+        }
+    }
+}
