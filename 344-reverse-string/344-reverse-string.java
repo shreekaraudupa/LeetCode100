5010cc75fd108fa534e11343fa3e860c11de31d6
shreekara  udupa <shreekaraudupa@gmail.com>
2022-05-01 16:54:57 +0530

    Time: 1 ms (83.63%), Space: 57.5 MB (28.26%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,3 +1,4 @@
+/*As the character array inpput is given swap the first and last element and then keep moving the start and end pointer to the middle*/
 class Solution {
     public void reverseString(char[] s) {
         int start = 0;
