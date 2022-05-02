434f062c12e9cd3f6863c065845838c5d13f273c
shreekara  udupa <shreekaraudupa@gmail.com>
2022-05-02 15:10:01 +0530

    Time: 2 ms (44.46%), Space: 64.7 MB (21.63%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,5 +1,23 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        if(nums.length <= 1) return;
+        k = k%nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i ++;
+            j --;
+        }
+    }
+    
+    /* This is done using extra space . we can do it without doing extra space too
+    public void rotate(int[] nums, int k) {
        int[] temp = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             temp[(i + k) % nums.length] = nums[i];
@@ -8,4 +26,5 @@ class Solution {
             nums[i] = temp[i];
         }
     }
+    */
 }
